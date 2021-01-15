3ca3d46e14f2c368ac7545b4f95ea0b43b6f3479
youcall911 <34089566+youcall911@users.noreply.github.com>
2021-01-15 16:31:28 +0800

    Update Blender render viewport frames by Luigi.py

--- Blender render viewport frames by Luigi.py
+++ Blender render viewport frames by Luigi.py
@@ -1,7 +1,9 @@
 # AUTHOR Luigi
 # VERSION 0.0.1
 #功能：直接渲染特定帧viewport画面
-#分为三步，首先关闭 viewport overlays,第二步跳转到指定的帧，第三部渲染并自动命名。二三步为循环。最后再打开overlay。
+#分为三步，首先关闭 viewport overlays,第二步跳转到指定的帧，第三步渲染并自动命名。二三步为循环。最后再打开overlay
+#默认使用jpg，文件更小
+#如果需要透明背景，请勾选“渲染属性 - 胶片 - 透明”，并使用png格式
 
 import bpy
 import time
@@ -14,11 +16,11 @@ for area in bpy.context.screen.areas:
                 break
 
 list = ['55', '90', '125'] #set the list of frame you want
-OUTPUT = "C:/Users/luigi/Pictures"   # Where to save Images
+OUTPUT = "C:/Users/.../Pictures"   # Where to save Images
 
 for i in list:
     bpy.context.scene.frame_set(int(i)) #jump to frame
-    timenow = time.strftime("%m%d %H%M%S", time.localtime()) #add timemark
-    filepath = OUTPUT + "/" + str(timenow) + ".png"
+    timenow = time.strftime("%Y%m%d %H%M%S", time.localtime()) #add timemark
+    filepath = OUTPUT + "/" + str(timenow)
     bpy.context.scene.render.filepath = filepath
     bpy.ops.render.opengl(write_still=True, view_context=True) #render viewport
