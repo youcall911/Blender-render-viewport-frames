106eb6c559e204944eed98903c7248906445a6b6
youcall911 <34089566+youcall911@users.noreply.github.com>
2021-06-10 10:08:41 +0800

    Update Blender render viewport frames by Luigi.py

--- Blender render viewport frames by Luigi.py
+++ Blender render viewport frames by Luigi.py
@@ -1,9 +1,13 @@
-# AUTHOR Luigi
-# VERSION 0.0.1
-#功能：直接渲染特定帧viewport画面
-#分为三步，首先关闭 viewport overlays,第二步跳转到指定的帧，第三步渲染并自动命名。二三步为循环。最后再打开overlay
-#默认使用jpg，文件更小
-#如果需要透明背景，请勾选“渲染属性 - 胶片 - 透明”，并使用png格式
+'''
+AUTHOR Luigi
+VERSION 0.0.2
+功能：直接渲染特定帧viewport画面
+分为三步，首先关闭 viewport overlays,第二步跳转到指定的帧，第三步渲染并自动命名。二三步为循环。最后再打开overlay
+默认使用jpg，文件更小
+如果需要透明背景：
+请勾选“渲染属性 - 胶片 - 透明”
+"输出属性 - 使用png格式，颜色：RGBA"
+'''
 
 import bpy
 import time
