7b961b45689f4d86d09da1676298d1f64702c4d2
youcall911 <34089566+youcall911@users.noreply.github.com>
2023-12-15 14:42:26 +0800

    Add files via upload

--- Blender render viewport with overlay.py
+++ Blender render viewport with overlay.py
@@ -1,6 +1,5 @@
 '''
 AUTHOR Luigi
-VERSION 0.0.2
 功能：直接渲染特定帧viewport画面，自动保存到我的图片
 分为三步，首先关闭 viewport overlays,第二步跳转到指定的帧，第三步渲染并自动命名。二三步为循环。最后再打开overlay
 默认使用jpg，文件更小
@@ -24,14 +23,25 @@ for area in bpy.context.screen.areas:
 
 # list = ['55', '90', '125'] #set the list of frame you want
 #OUTPUT = "C:/Users/Administrator/Pictures"   # Where to save Images
-OUTPUT = os.path.join(os.path.expanduser('~'), 'Pictures')   # 自动保存到我的图片
+OUTPUT = os.path.join(os.path.expanduser('~'), 'Pictures')  
+# 自动保存到我的图片
+
+#读取当前文件名
+filename = bpy.path.basename(bpy.context.blend_data.filepath)
+#删除blend后缀
+filename = filename.strip('.blend')
 
 #读取当前相机名称
 cameraname = bpy.context.scene.camera.name
 
 #for i in list:
 #bpy.context.scene.frame_set(int(i)) #jump to frame
-timenow = time.strftime("%Y%m%d %H%M%S", time.localtime()) #add timemark
-filepath = OUTPUT + "/" + str(timenow) 
+timenow = time.strftime("%Y%m%d %H%M%S", time.localtime()) 
+#日期
+
+filepath = OUTPUT + "/" + str(timenow) + ' ' + str(cameraname) + ' ' + str(filename)
+#生成路径
+
 bpy.context.scene.render.filepath = filepath
-bpy.ops.render.opengl(write_still=True, view_context=True) #render viewport
+bpy.ops.render.opengl(write_still=True, view_context=True) 
+#render viewport
